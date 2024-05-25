9bb725e6106793d1b19c2ed7b9e1ccfea2c93a72
WeslleySoaresm <weslley.trabalheconosco@gmail.com>
2024-05-25 09:54:04 +0000

    criando o codigo 02 de python

---
+++ codigo02.py
@@ -0,0 +1,14 @@
+# Solicita ao usuário que insira uma string e armazena na variável 'texto'
+texto = input("Por favor, insira uma string: ")
+
+# Solicita ao usuário que insira um número inteiro e armazena na variável 'numero'
+# Utiliza a função int() para converter a entrada do usuário em um número inteiro
+numero = int(input("Por favor, insira um número inteiro: "))
+
+# Repete a string 'texto' o número de vezes informado pelo usuário
+# Utiliza o operador '*' para repetir a string
+resultado = texto * numero
+
+# Exibe a string resultante da repetição
+print("A string resultante é:", resultado)
+
